886049b82ac38f640796ac81e4517f9009553786
cinwald <cinwald@gmail.com>
2015-08-27 15:41:48 +0000

    updated Hello World

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//Charles Inwald
+//Lab 001
+//Hello World - Charles's first Java Program
+//    -compiled with javac HelloWorldjava
+//    -ran with java HelloWorld
+
+// I define a class with:
+public class HelloWorld{
+    
+//  main method?
+  public static void main(String[] args) {
+      
+//  print Hello World
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2015!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+
+  }
+}
