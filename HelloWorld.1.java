5731900ef884d6d14990d3267023198e9dd53a78
cinwald <cinwald@gmail.com>
2015-09-01 15:19:22 +0000

    uploaded first homework

---
+++ HelloWorld.1.java
@@ -0,0 +1,22 @@
+//Charles Inwald
+
+
+// I define a class with:
+public class HelloWorld{
+    
+//  main method?
+  public static void main(String[] args) {
+      
+//  print Hello World
+    float processsorSpeed;
+    byte bumberProcessors;
+    float computerSpeed;
+    
+    computerSpeed = processorSpeed * numberProcessors;
+    
+    System.out.println("Computer Speed = " + computerSpeed)
+    
+    
+
+  }
+}
---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Charles Inwald
+//hw01
+//WelcomeClass
+
+// I define the class with:
+public class WelcomeClass{
+    
+//  main method
+  public static void main(String[] args) {
+      
+//  print Welcome Screen
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-C--C--I--2--1--9-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("Prior to this class, my only programming experience was web design over the summer. My website is charlesinwald.com in case you're interested.");
+    
+
+  }
+}
